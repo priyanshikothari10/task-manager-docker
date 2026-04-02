c36297c2c9d88adf18b5cee26111bb9c486267a5
Priyanshi Kothari <priyanshikothari522@gmail.com>
2026-04-02 19:43:32 +0530

    Initial commit - Dockerized Task Manager App

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let tasks = [];
+
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+app.post("/tasks", (req, res) => {
+  const task = req.body.task;
+  tasks.push(task);
+  res.json({ message: "Task added" });
+});
+
+app.listen(5000, () => {
+  console.log("Backend running on port 5000");
+});
+app.get("/add", (req, res) => {
+  tasks.push("Learn Docker");
+  res.send("Task added");
+});
